b44921d76242b4e42f873a1fbcd70a6866a54f7b
KieranGliver <kieran.glovjunk@gmail.com>
2025-01-27 11:01:57 -0500

    Fixed manual aiming bug

--- Scripts/Buildings/ManualWeapon.gd
+++ Scripts/Buildings/ManualWeapon.gd
@@ -6,7 +6,7 @@ func _input(event):
 	if event is InputEventMouse:
 		if event is InputEventMouseMotion:
 			# I don't really know why I have to add 90 degress
-			rotation = (get_global_mouse_position()).angle() + SPRITE_ROTATION_OFFSET
+			rotation = (get_global_mouse_position()-global_position).angle() + SPRITE_ROTATION_OFFSET
 
 func _unhandled_input(event):
 	if event is InputEventMouse:
@@ -16,6 +16,6 @@ func _unhandled_input(event):
 
 func fire_projectile():
 	var projectile_instant = projectile.instantiate()
-	projectile_instant.set_target(get_global_mouse_position())
+	projectile_instant.set_target(get_global_mouse_position()-global_position)
 	projectile_instant.damage = damage_val
 	get_parent().add_child(projectile_instant)
